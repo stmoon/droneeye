6f3746ff29cab156f718f6fe370f048ec2f4b2c2
stmoon <munhoney@gmail.com>
2019-06-12 17:55:47 +0900

    modify bbox mechanism

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from __future__ import division
 
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+
 from models import *
 from utils.utils import *
 from utils.datasets import *
@@ -18,6 +21,30 @@ from torchvision import datasets
 from torchvision import transforms
 from torch.autograd import Variable
 import torch.optim as optim
+
+def scaling_bbox(img_path, bbox) :
+
+    # read original bbox
+    x1, y1, x2, y2 = bbox
+
+    # read img 
+    img = np.array(Image.open(img_path))
+
+    # The amount of padding that was added
+    pad_x = max(img.shape[0] - img.shape[1], 0) * (opt.img_size / max(img.shape))
+    pad_y = max(img.shape[1] - img.shape[0], 0) * (opt.img_size / max(img.shape))
+
+    # Image height and width after padding is removed
+    unpad_h = opt.img_size - pad_y
+    unpad_w = opt.img_size - pad_x
+
+    # Rescale coordinates to original dimensions
+    box_h = ((y2 - y1) / unpad_h) * img.shape[0]
+    box_w = ((x2 - x1) / unpad_w) * img.shape[1]
+    y1 = ((y1 - pad_y // 2) / unpad_h) * img.shape[0]
+    x1 = ((x1 - pad_x // 2) / unpad_w) * img.shape[1]
+
+    return x1,y1,box_w,box_h
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", type=int, default=16, help="size of each image batch")
@@ -38,7 +65,7 @@ cuda = torch.cuda.is_available() and opt.use_cuda
 
 # Get data configuration
 data_config = parse_data_config(opt.data_config_path)
-test_path = data_config["valid"]
+test_path = data_config["test"]
 num_classes = int(data_config["classes"])
 
 
@@ -89,14 +116,13 @@ for batch_i, (imgs_path, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc=
             _, fp =  os.path.split(file_path)
             frame_index = os.path.splitext(fp)[0].split('_')[-1]
             
-            print(file_path)
-            print(file_path.split('/')[-2])
             output_file = open('output/'+file_path.split('/')[-2]+'.txt','a')
             for i in range(len(pred_boxes)) :
                 result = []
                 result.append(int(frame_index))
                 result.append(-1)
-                result += list(map(int,pred_boxes[i].tolist()[:-1]))
+                #result += list(map(int,pred_boxes[i].tolist()[:-1]))
+                result += scaling_bbox(file_path, pred_boxes[i].tolist()[:-1])
                 result.append(round(scores[i],4))
                 result.append(int(pred_labels[i]))
                 result.append(-1)
