7f43531cf602d10292f954912fa4a757060482e3
stmoon <munhoney@gmail.com>
2019-07-09 15:04:13 +0900

    use python nms instead of gpu nms or softnms

--- m2det/utils/nms_wrapper.py
+++ m2det/utils/nms_wrapper.py
@@ -7,6 +7,7 @@
 
 from .nms.cpu_nms import cpu_nms, cpu_soft_nms
 from .nms.gpu_nms import gpu_nms
+from .nms.py_cpu_nms import py_cpu_nms
 
 
 # def nms(dets, thresh, force_cpu=False):
@@ -25,7 +26,12 @@ def nms(dets, thresh, force_cpu=False):
 
     if dets.shape[0] == 0:
         return []
+
+    return py_cpu_nms(dets, thresh)
+
+    '''
     if force_cpu:
         return cpu_soft_nms(dets, thresh, method = 1)
         #return cpu_nms(dets, thresh)
     return gpu_nms(dets, thresh)
+    '''
